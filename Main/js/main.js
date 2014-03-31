38fe1e10ae3d1e612fa355449f0b73992762a6f6
rashaunamead <rashauna.mead@gmail.com>
2014-03-31 12:20:24 -0500

    Navigation
    Added foundation framework.  Added navigation bar and temp icons.

--- Main/js/main.js
+++ Main/js/main.js
@@ -4,7 +4,7 @@
   
   var map = L.map('map');
     // tiles can change once we know our basemap 
-    L.tileLayer('https://{s}.tiles.mapbox.com/v3/rashauna.hb2aepgd/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tiles.mapbox.com/v3/rashauna.hlkken8n/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a> <a href="http://http://leafletjs.com"> Leaflet </a> Tiles <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18,
       minzoom: 5,
