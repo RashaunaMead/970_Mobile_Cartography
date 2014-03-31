44cdc34bd0f61c9e097a70b087a059410e67f26a
rashaunamead <rashauna.mead@gmail.com>
2014-03-31 18:41:50 -0500

    adding pages for web.io

---
+++ README.md
@@ -0,0 +1,6 @@
+Mobile_Cartography
+==========================
+Code Examples Folder can be used for our code that we are working on / playing around before we know how we want to integrate it to the map
+
+In the main folder is the index.html for our mobile map.   
+
---
+++ js/main.js
@@ -0,0 +1,17 @@
+ window.onload = loadmap();
+  
+ function loadmap(){
+  
+  var map = L.map('map');
+    // tiles can change once we know our basemap 
+    L.tileLayer('https://{s}.tiles.mapbox.com/v3/rashauna.hlkken8n/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a> <a href="http://http://leafletjs.com"> Leaflet </a> Tiles <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      minzoom: 5,
+    }).addTo(map);
+
+    // initial zoom & set map coords, these will change 
+    map.setView([43.07790859834721, -89.37177476473153], 16);
+  
+}
+
