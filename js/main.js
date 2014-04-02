851400d2a956457748c89f3aa2e0c55c896f80fe
Brian Davidson <briandaviddavidson@yahoo.com>
2014-04-02 12:11:21 -0500

    Merge branch 'master' of https://github.com/RashaunaMead/970_Mobile_Cartography
    Conflicts:
    Main/css/style.css

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
+//main.js initiates the map and runs the functionality of the map 
+var map;
  window.onload = loadmap();
-  
+
+
  function loadmap(){
-  
-  var map = L.map('map');
+ 
+  map = L.map('map');
     // tiles can change once we know our basemap 
     L.tileLayer('https://{s}.tiles.mapbox.com/v3/rashauna.hlkken8n/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a> <a href="http://http://leafletjs.com"> Leaflet </a> Tiles <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18,
       minzoom: 5,
     }).addTo(map);
-
     // initial zoom & set map coords, these will change 
-    map.setView([43.07790859834721, -89.37177476473153], 16);
-  
+    map.setView([43.07790859834721, -89.37177476473153], 14);
+    map.attributionControl.setPosition('topright');
+    
 }
+
+// calls the find me function 
+$("li.findMe").click(function(){
+    	GetLocation(map);
+    })
 
