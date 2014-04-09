998ad49cb4163b1c1cf6c96dca36a1ac57504816
rashaunamead <rashauna.mead@gmail.com>
2014-04-09 14:17:30 -0500

    Audio Tag Placed
    Audio div placed above nav for desktop and below for mobile.
    
    Switch Tiles now moves up for mobile, resolving conflict with nag bar.

--- js/responsive.js
+++ js/responsive.js
@@ -20,13 +20,13 @@ $(window).load(function() {
   var winWidth = $(window).width();
 
   switchElements(winWidth);
-   //  $("#container").hide();
-   // $(".ontop").hide()
-   // $("#splash a").click(function(){
-   //   $("#container").show();
-   //   $("#splash").hide();
-   //  $(".ontop").show();
- // })
+    $("#container").hide();
+   $(".ontop").hide()
+   $("#splash a").click(function(){
+     $("#container").show();
+     $("#splash").hide();
+    $(".ontop").show();
+ })
 
 });
  
