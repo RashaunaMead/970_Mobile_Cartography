17561ae575e424ef8ce4bed72f925d7255d4859a
rashaunamead <rashauna.mead@gmail.com>
2014-04-10 07:46:19 -0500

    poi for zoom update

---
+++ data/POI.js
@@ -0,0 +1,78 @@
+var POI = {
+	"features": {
+		"Candy_Factory":{
+			"geometry": { 
+				"type": "Point", 
+				"coordinates": [43.07805483986813 ,-89.371953550849724] 
+				}, 
+		"properties": { 
+			"id": 4, "title": "Madison Candy Factory",	
+			"icon": {
+				"iconUrl": "images/production24design2.png",
+				"iconSize": [24, 24], // size of the icon
+				"iconAnchor": [12, 12], // point of the icon which will correspond to marker's location
+				"popupAnchor": [0, -12] // point from which the popup should open relative to the iconAnchor
+		
+				},
+			 "Descriptio": "“Think about how the current businesses in this building produce value, and contrast these post-Fordist forms of production to the Fordist mode of production that used to inhabit it.", 
+			 "PhotoLink": null, "Address": "744 Williamson St.", "iconLink24": null, "iconLink36": null 
+		} //end properties		
+
+			
+		},
+		"Wil_Mar":{
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [ 43.079849673461318,-89.366976240603208 ] 
+			}, 
+			"properties": { 
+				"id": 3, "title": "Wil-Mar Neighborhood Center", 
+				"icon": {
+					"iconUrl": "images/housing24design2.png",
+					"iconSize": [24, 24], // size of the icon
+					"iconAnchor": [12, 12], // point of the icon which will correspond to marker's location
+					"popupAnchor": [0, -12] // point from which the popup should open relative to the iconAnchor
+					
+					},
+				"Descriptio": ". Built as a church and now a community center, the building provides a good opportunity to reflect on how the neighborhood has changed.", "PhotoLink": null, "Address": "953 Jenifer St.", "iconLink24": null, "iconLink36": null 
+			}  //end properties
+		},
+		"Power_Plant":{
+			"geometry": { 
+				"type": "Point", 
+				"coordinates": [  43.079766795017484,-89.375201651182024 ] 
+			},
+			"properties": { 
+				"id": 2, "title": "Power Plant", 
+				"icon": {
+					"iconUrl": "images/energy24design2.png",
+					"iconSize": [24, 24], // size of the icon
+					"iconAnchor": [12, 12], // point of the icon which will correspond to marker's location
+					"popupAnchor": [0, -12] // point from which the popup should open relative to the iconAnchor
+				
+					},
+				"Descriptio": "Think through questions about energy flows through homes and business, inequalities related to the electric grid, and alternative forms of power.", "PhotoLink": "images\/powerPlant.jpg", "Address": "722 E Main St.", "iconLink24": null, "iconLink36": null
+			} //end properties  
+		},
+		"Shoe_Factory":{
+			"geometry": { 
+				"type": "Point", 
+				"coordinates": [ 43.081285046311976,-89.378615022118979 ] 
+			}, 
+
+			"properties": { 
+				"id": 0, "title": "Badger State Shoe Factory", 
+				"icon": {
+					"iconUrl": "images/labor24design2.png",
+					"iconSize": [24, 24], // size of the icon
+					"iconAnchor": [12, 12], // point of the icon which will correspond to marker's location
+					"popupAnchor": [0, -12] // point from which the popup should open relative to the iconAnchor
+					
+					},
+				"Descriptio": "(From Wikipedia) The building was constructed in 1910 after the company had originally expanded its production to Madison in 1900.  designated a landmark by the Madison Landmarks Commission in 1989.", "PhotoLink": "images\/BadgerStateShoeFactory1.jpg", "Address": "123 N Blount", "iconLink24": null, "iconLink36": null 
+			} //end properties 
+		},
+	}//end features
+};//close var
+
+
