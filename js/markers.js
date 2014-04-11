b2df9ecb5c665a591763b75588341f81093ecfdd
Guiming <gzhang45@wisc.edu>
2014-04-11 10:38:49 -0500

    changes made in markers.js for slideshow

--- js/markers.js
+++ js/markers.js
@@ -1,4 +1,28 @@
 //this file created by Caroline. 
+
+//orbit settings
+$(document).foundation({
+    orbit: {
+        animation: 'slide',
+        navigation_arrows: true,
+        circular: false,
+        timer: false,
+		swipe: false,
+        next_class: 'orbit-next',
+        prev_class: 'orbit-prev'
+    }
+});	
+
+//resize the pop-up modal window
+$('.reveal-modal').on('opened', function () {
+
+	$(".twentytwenty-container").twentytwenty();
+	$(window).trigger('resize');  
+});
+	
+//hide PREV BUTTON initially
+$('.orbit-prev').hide();
+
 //this function adds custom icons to the map, drawing the path to the image from the geojson 
 function addMarkers (map) {
 
@@ -29,6 +53,37 @@ function openInfoScreen (feature){
 	var infoScreen = document.getElementById("slideshowModal");
 	//infoScreen.style.visibility = "visible";
 	document.getElementById("show_title").textContent = feature.properties.title;
+	
+	// set image text for the first slide
+	if($("#slideshow_texts").innerHTML==null){
+		$("#slideshow_texts").html(feature.properties.imageSet[0].image_texts);
+	}
+
+	//-------- make changes after each slide transition --------------
+	$("#slideshow_images").on("after-slide-change.fndtn.orbit", function(event, orbit) {  
+
+		// image text changes with slide
+		$("#slideshow_texts").html(feature.properties.imageSet[orbit.slide_number].image_texts);
+
+		// CLOSE BUTTON shows only on the last slide
+		if(orbit.slide_number == orbit.total_slides - 1){
+			$(".close-reveal-modal").html("&#215;");
+		}
+		else{
+			$(".close-reveal-modal").html("");
+		}
+
+		// hide/show PREV/NEXT BUTTON
+		$('.orbit-next').show();
+		$('.orbit-prev').show();
+
+		if(orbit.slide_number == 0){
+			$('.orbit-prev').hide();
+		}
+		if(orbit.slide_number == orbit.total_slides - 1){
+			$('.orbit-next').hide();
+		}
+	});
 	
 	$("#slideshowModal").foundation("reveal", "open");
 	
