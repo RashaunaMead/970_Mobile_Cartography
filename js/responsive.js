25188d5d226af90aebb345671883f530385c7956
Brian Davidson <briandaviddavidson@yahoo.com>
2014-04-27 15:00:27 -0500

    Corrected audio on Mobile View
    Fixed audio in responsive.js

--- js/responsive.js
+++ js/responsive.js
@@ -38,8 +38,8 @@ $(window).load(function() {
      $(".ontop").show();
      $("nav").show();
      //Moved to responsive function
-     // $("audio").load();
-     // $("audio").prop('muted', false);
+     $("audio").load();
+     $("audio").prop('muted', false);
 
      if(winWidth<=midBreakPoint){
      map.setView([43.076364, -89.384336], 14);
